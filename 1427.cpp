f4692da8d08969c11adfa339acbd454a08477f38
GangHyun-Park <66900601+GangHyun-Park@users.noreply.github.com>
2020-09-27 16:32:39 +0900

    Create 1427.cpp
    소트인사이드

---
+++ 1427.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	sort(str.begin(), str.end());
+	reverse(str.begin(), str.end());
+
+	cout << str;
+
+	return 0;
+}
